ec015cbe2c82c183236ad64d9003f67cf3a12ca5
windingwind <33902321+windingwind@users.noreply.github.com>
2024-11-18 15:03:16 +0100

    add: disable by language
    resolve: #5

--- addon/prefs.js
+++ addon/prefs.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 pref("__prefsPrefix__.enableBionicReader", true);
+pref("__prefsPrefix__.disableForLanguages", "");
 pref("__prefsPrefix__.enableReaderToolbarButton", true);
 pref("__prefsPrefix__.parsingContrast", 1);
 pref("__prefsPrefix__.parsingOffset", 0);
--- src/utils/status.ts
+++ src/utils/status.ts
@@ -6,9 +6,28 @@ function getCurrentItemStatus(itemID: number): boolean {
   const bionicTemporaryData = getPrefJSON("bionicTemporaryData");
   let currentStatus = bionicTemporaryData[itemID];
   if (currentStatus === undefined) {
+    const item = Zotero.Items.getTopLevel([Zotero.Items.get(itemID)])[0];
+    if (isDisabledLanguage(item.getField("language"))) {
+      return false;
+    }
     currentStatus = !!getPref("enableBionicReader");
   }
   return currentStatus;
+}
+
+function isDisabledLanguage(lang: string): boolean {
+  const disabledLanguages = (
+    (getPref("disableForLanguages") as string) || ""
+  ).toLocaleLowerCase();
+  if (!disabledLanguages) {
+    return false;
+  }
+  const langSplitter = lang.includes("-") ? "-" : "_";
+  const langPart = lang.split(langSplitter)[0].toLocaleLowerCase();
+  if (!langPart) {
+    return false;
+  }
+  return disabledLanguages.includes(langPart);
 }
 
 function toggleCurrentItemStatus(itemID: number): void {
